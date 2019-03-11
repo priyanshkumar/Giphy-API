009ea175fff2e4cad092721dc82565492f24475b
Priyansh <pradadiya667@gmail.com>
2019-03-11 17:44:31 -0400

    added functionality to render the list of animals to index.html

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  var animals = ["cat", "dog", "snake", "lion", "tiger", "pengiun"];
+
+  function renderButton() {
+    $(".buttons").empty();
+
+    for (var i = 0; i < animals.length; i++) {
+      var a = $("<button>");
+
+      a.addClass("animal");
+
+      a.attr("data-name", animals[i]);
+
+      a.text(animals[i]);
+
+      $(".buttons").append(a);
+    }
+  }
+
+  console.log("hiiiiii");
+  renderButton();
+});
